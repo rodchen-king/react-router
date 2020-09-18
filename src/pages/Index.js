5b1f19585c88a178ede67361334dbda85127a220
rodchen <rodchen_king@outlook.com>
2020-09-18 20:00:04 +0800

    1. history listen

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,24 @@
 
 import React, { Component } from 'react';
 
+let UNLISTEN;
+
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+    componentDidMount(){
+        // UNLISTEN变量用来接收解绑函数
+        UNLISTEN = this.props.history.listen(route => { 
+            console.log(route); 
+        });
+    }
+    componentWillUnmount(){
+        UNLISTEN && UNLISTEN(); // 执行解绑
+    }
+
     render() { 
         return (  <h2>JSPang.com</h2> );
     }
