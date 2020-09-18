0de4b4373c0b1aa45cbc50ac7990cbc6bf4e2a37
rodchen <rodchen_king@outlook.com>
2020-09-18 14:14:18 +0800

    1. 嵌套路由

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,21 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import Index from './pages/Index';
-import List from './pages/List';
-import NotFoundPage from './pages/NotFoundPage'
+import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
+import Index from './pages/Index'
+//--关键代码------------start
+import Video from './pages/Video'
+import WorkPlace from './pages/WorkPlace'
+//--关键代码------------end
+import './index.css'
 
 function AppRouter() {
-  return (
-    <Router>
-        <ul>
-            <li> <Link to="/">首页</Link> </li>
-            <li><Link to="/list/123?name=rodchen">列表</Link> </li>
-        </ul>
-        <Redirect from="" to="/home" />
-        <Route path="/home" exact component={Index} />
-        <Route path="/list/:id" exact component={List} />
-        <Route path="/404" component={NotFoundPage} />
-    </Router>
-  );
-}
-export default AppRouter;
+    return (
+      <Router>
+          <div className="mainDiv">
+            <div className="leftNav">
+                <h3>一级导航</h3>
+                <ul>
+                    <li> <Link to="/">博客首页</Link> </li>
+                    {/*--关键代码------------start*/}
+                    <li><Link to="/video/">视频教程</Link> </li>
+                    {/*--关键代码------------end*/}
+                    <li><Link to="/workplace">职场技能</Link> </li>
+                </ul>
+            </div>
+            
+            <div className="rightMain">
+                <Route path="/"  exact component={Index} />
+                 {/*--关键代码------------start*/}
+                <Route path="/video/"   component={Video} />
+                <Route path="/workplace/"   component={WorkPlace} />
+                 {/*--关键代码------------end*/}
+            </div>
+          </div>
+      </Router>
+    );
+  }
+  
+  export default AppRouter;
+  
+
