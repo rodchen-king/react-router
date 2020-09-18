1ab7887d4edf64929d8dc5f5301262e923865896
rodchen <rodchen_king@outlook.com>
2020-09-18 19:14:15 +0800

    1. withRouter

--- src/pages/video/Flutter.js
+++ src/pages/video/Flutter.js
@@ -1,5 +1,26 @@
 import React from "react";
-function Flutter(){
-    return (<h2>我是Flutter页面</h2>)
+import {withRouter} from 'react-router-dom' //引入withRouter
+
+function Flutter(props){
+  console.log(props.match)
+
+    return (
+      <div>
+        <h2>我是Flutter页面</h2>
+        <TestWithRouter />
+      </div>
+    )
 }
+
+// function TestWithRouter(props) {
+//   console.log(props.match)
+//     return (<h2>我是testWithRouter页面</h2>)
+// }
+
+
+const TestWithRouter = withRouter((props) => {
+  console.log(props.match)
+  return (<h2>我是testWithRouter1页面</h2>)
+})
+
 export default Flutter;
