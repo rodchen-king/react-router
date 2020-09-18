35c14537aeb4ec0f4820eded3e84ef9bbd3af0f6
rodchen <rodchen_king@outlook.com>
2020-09-18 11:10:33 +0800

    1. 修改基础项目结构

---
+++ src/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Index from './pages/Index';
+import List from './pages/List';
+import NotFoundPage from './pages/NotFoundPage'
+
+function AppRouter() {
+  return (
+    <Router>
+        <ul>
+            <li> <Link to="/">首页</Link> </li>
+            <li><Link to="/list/">列表</Link> </li>
+        </ul>
+        <Route path="/" exact component={Index} />
+        <Route path="/list/" exact component={List} />
+        <Route path="/404" component={NotFoundPage} />
+    </Router>
+  );
+}
+export default AppRouter;
