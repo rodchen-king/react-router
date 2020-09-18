9dcedbd496b30e97216e59d7615dd179267f9802
rodchen <rodchen_king@outlook.com>
2020-09-18 16:29:57 +0800

    1. BrowserRouter & switch

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,36 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch  } from "react-router-dom";
 import Index from './pages/Index'
-//--关键代码------------start
 import Video from './pages/Video'
 import WorkPlace from './pages/WorkPlace'
-//--关键代码------------end
 import './index.css'
 
 function AppRouter() {
     return (
-      <Router>
+      <BrowserRouter>
           <div className="mainDiv">
             <div className="leftNav">
                 <h3>一级导航</h3>
                 <ul>
                     <li> <Link to="/">博客首页</Link> </li>
-                    {/*--关键代码------------start*/}
                     <li><Link to="/video/">视频教程</Link> </li>
-                    {/*--关键代码------------end*/}
                     <li><Link to="/workplace">职场技能</Link> </li>
                 </ul>
             </div>
             
             <div className="rightMain">
-                <Route path="/"  exact component={Index} />
-                 {/*--关键代码------------start*/}
-                <Route path="/video/"   component={Video} />
+              {/* <Switch> */}
+                <Route path="/" exact component={Index} />
+                <Route path="/video/" component={Video} />
                 <Route path="/workplace/"   component={WorkPlace} />
-                 {/*--关键代码------------end*/}
+              {/* </Switch>   */}
             </div>
           </div>
-      </Router>
+      </BrowserRouter>
     );
   }
   
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {  Route, Link  } from "react-router-dom";
+import {  Route, Link, Switch  } from "react-router-dom";
 import Reactpage from './video/ReactPage'
 import Vue from './video/Vue'
 import Flutter from './video/Flutter'
 
 
-function Video(){
+function Video(props){
     return (
         <div>
             <div className="topNav">
@@ -17,9 +17,12 @@ function Video(){
             </div>
             <div className="videoContent">
                 <div><h3>视频教程</h3></div>
-                <Route path="/video/reactpage/"   component={Reactpage} />
-                <Route path="/video/vue/"   component={Vue} />
-                <Route path="/video/flutter/"   component={Flutter} />
+                <button onClick={() => {props.history.push('/')}}>跳转首页</button>
+                <Switch>
+                    <Route path="/video/reactpage/"   component={Reactpage} />
+                    <Route path="/video/vue/"   component={Vue} />
+                    <Route path="/video/flutter/"   component={Flutter} />
+                </Switch>
             </div>
         </div>
     )
